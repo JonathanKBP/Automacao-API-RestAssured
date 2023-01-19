0677d7781fd84df385e2571ac2dcc75613a1b533
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-01-18 22:50:13 -0300

    Teste: Não deve cadastrar movimentação futura

--- src/main/java/br/com/jonathan/rest/tests/BarrigaTest.java
+++ src/main/java/br/com/jonathan/rest/tests/BarrigaTest.java
@@ -76,16 +76,7 @@ public class BarrigaTest extends BaseTest {
 	
 	@Test
 	public void deveInserirMovimentacaoComSucesso() {
-		Movimentacao mov = new Movimentacao();
-		mov.setConta_id(1559037);
-//		mov.setUsuario_id(usuario_id);
-		mov.setDescricao("Descricao da movimentacao");
-		mov.setEnvolvido("Envolvido na mov");
-		mov.setTipo("REC");
-		mov.setData_transacao("01/01/2000");
-		mov.setData_pagamento("10/05/2010");
-		mov.setValor(100f);
-		mov.setStatus(true);
+		Movimentacao mov = getMovimentacaoValida();
 		
 		given()
 			.header("Authorization", "JWT " + TOKEN)
@@ -117,4 +108,35 @@ public class BarrigaTest extends BaseTest {
 					"Situação é obrigatório"
 				));
 		}
+	
+	@Test
+	public void naoDeveInserirMovimentacaoComDataFutura() {
+		Movimentacao mov = getMovimentacaoValida();
+		mov.setData_transacao("20/10/2023");
+		
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body(mov)
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(1))
+			.body("msg", hasItem("Data da Movimentação deve ser menor ou igual à data atual"));
+		}
+	
+	private Movimentacao getMovimentacaoValida() {
+		Movimentacao mov = new Movimentacao();
+		mov.setConta_id(1559037);
+//		mov.setUsuario_id(usuario_id);
+		mov.setDescricao("Descricao da movimentacao");
+		mov.setEnvolvido("Envolvido na mov");
+		mov.setTipo("REC");
+		mov.setData_transacao("01/01/2000");
+		mov.setData_pagamento("10/05/2010");
+		mov.setValor(100f);
+		mov.setStatus(true);
+		
+		return mov;
+	}
 }
